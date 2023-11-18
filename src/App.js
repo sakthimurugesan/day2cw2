9d10645201ef0274fb4cf4b87c2b72d7aeba8859
SAKTHI M <msakthi150@gmail.com>
2023-11-18 09:47:11 +0530

    Changes to be committed

--- src/App.js
+++ src/App.js
@@ -4,20 +4,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={{
+        color:'green'
+      }}>Hello world</h1>
+      <br></br>
+        <p style={{
+          display:'block',
+          color:'darkblue',
+          fontSize:'16px',
+          padding:'5px',
+          backgroundColor:"red"
+
+        }}>Hello</p>
+    
     </div>
   );
 }
